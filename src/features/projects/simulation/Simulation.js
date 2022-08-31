eb47b07cbd5f97eed1e5711f8364c0b1159ceddf
Daniel Cohen <daniel.m.cohen@berkeley.edu>
2022-08-31 19:55:14 -0400

    steal david's website

---
+++ src/features/projects/simulation/Simulation.js
@@ -0,0 +1,11 @@
+import Thumbnail from './thumbnail.png';
+
+const title = 'Soft-body Tetrahedral Simulation';
+const description = `Soft-body simulation based on tetrahedral meshes, implemented in C++.`;
+
+export default {
+  title,
+  description,
+  thumbnail: Thumbnail,
+  href: 'https://www.youtube.com/watch?v=sQXepdWgsjE',
+};
---
+++ src/features/projects/stylit/StyLit.js
@@ -0,0 +1,11 @@
+import Thumbnail from './thumbnail.png';
+
+const title = 'StyLit Paper Reimplementation';
+const description = `An implementation of Fišer et al.’s StyLit algorithm for CSCI 2240: Interactive Computer Graphics.`;
+
+export default {
+  title,
+  description,
+  thumbnail: Thumbnail,
+  href: 'https://github.com/dcharata/StyLit',
+};
