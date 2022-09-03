386adfa7718e418bba3b26e78876c80840e203ee
Daniel Cohen <daniel.m.cohen@berkeley.edu>
2022-09-03 10:03:16 -0400

    mistakes were made

--- src/features/projects/projects.js
+++ src/features/projects/projects.js
@@ -2,12 +2,10 @@ import DynamicCameraLocalization from './dynamic-camera-relocalization/DynamicCa
 import Mesh from './mesh/Mesh';
 import Museum from './museum/Museum';
 import PathTracer from './path-tracer/PathTracer';
-import Point2Mesh from './point2mesh/Point2Mesh';
 import Simulation from './simulation/Simulation';
 import StyLit from './stylit/StyLit';
 
 export default [
-  Point2Mesh,
   StyLit,
   PathTracer,
   DynamicCameraLocalization,
