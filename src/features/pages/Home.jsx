582fdb549ffe8a9da8944172379df2d35b29fc4f
Daniel Cohen <daniel.m.cohen@berkeley.edu>
2023-04-17 13:57:45 -0400

    apostrophe typo fix

--- src/features/pages/Home.jsx
+++ src/features/pages/Home.jsx
@@ -18,16 +18,16 @@ const Home = () => (
           <a href="https://www.microsoft.com/en-us/research/lab/microsoft-research-new-england/">
             Microsoft Research New England
           </a>
-          . In fall 2023 I will be joining Northwestern University's Kellogg
-          School of Management as a PhD student in Managerial Economics and
-          Strategy. I am grateful to be supported in my research by the National
-          Science Foundation's Graduate Research Fellowship Program. \r\n
-          Academically, I&apos;m interested in using novel data collection and
-          analysis methods to understand how households make financial
-          decisions. Personally, I&apos;m interested in hiking, cycling,
-          mountain biking, climbing, and obsessing over the perfect espresso. I
-          graduated from UC Berkeley in 2021 with dual degrees in Economics and
-          Data Science.
+          . In fall 2023 I will be joining Northwestern University&apos;s
+          Kellogg School of Management as a PhD student in Managerial Economics
+          and Strategy. I am grateful to be supported in my research by the
+          National Science Foundation&apos;s Graduate Research Fellowship
+          Program. \r\n Academically, I&apos;m interested in using novel data
+          collection and analysis methods to understand how households make
+          financial decisions. Personally, I&apos;m interested in hiking,
+          cycling, mountain biking, climbing, and obsessing over the perfect
+          espresso. I graduated from UC Berkeley in 2021 with dual degrees in
+          Economics and Data Science.
         </div>
         <div className="mt-2">
           <Link to="/research">Research</Link>
