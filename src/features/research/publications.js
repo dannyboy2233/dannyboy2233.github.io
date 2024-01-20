04782008bd7f539f2e8ba4076e5972b95e6144ed
Daniel Cohen <daniel.m.cohen@berkeley.edu>
2024-01-20 09:51:17 -0600

    update link to optimalNPI paper pdf

--- src/features/research/publications.js
+++ src/features/research/publications.js
@@ -7,7 +7,8 @@ import {
 
 export const OptimalNPI = {
   title: 'When Do "Nudges" Increase Welfare?',
-  paperUrl: 'https://www.dropbox.com/s/v4g6zqhmrrz3njs/OptimalNPI.pdf?dl=0',
+  paperUrl:
+    'https://www.dropbox.com/scl/fi/r7esbzpyeu5pwojxi83tc/NudgeWelfare.pdf?rlkey=d7glp5wklv2uu1nudtjr5eqn3&dl=0',
   repoUrl:
     'https://www.dropbox.com/s/knmfo3s9x4u7vo5/OptimalNPI_replication.zip?dl=0',
   authors: [HuntAllcott, DanielCohen, WilliamMorrison, DmitryTaubinsky],
