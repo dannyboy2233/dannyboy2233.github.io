64c1b3c3fd0ea3b240ea43565c5af6efdba42b6c
Daniel Cohen <daniel.m.cohen@berkeley.edu>
2026-05-06 09:38:04 -0600

    update homepage

--- src/features/pages/Home.jsx
+++ src/features/pages/Home.jsx
@@ -18,22 +18,55 @@ const Home = () => (
           at Northwestern University&apos;s Kellogg School of Management. Before
           that, I was a research fellow in the Economics and Computation group
           at{' '}
-          <a href="https://www.microsoft.com/en-us/research/lab/microsoft-research-new-england/">
+          <a
+            href="https://www.microsoft.com/en-us/research/lab/microsoft-research-new-england/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Microsoft Research New England
           </a>
-          . I am grateful to be supported in my graduate studies by the National
-          Science Foundation&apos;s{' '}
-          <a href="https://www.nsfgrfp.org/">
+          . I&apos;m grateful to be supported in my graduate studies by the
+          National Science Foundation&apos;s{' '}
+          <a
+            href="https://www.nsfgrfp.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Graduate Research Fellowship Program
           </a>{' '}
-          under Grant No. DGE-2234667.
+          under Grant No. DGE-2234667. I&apos;m also grateful for
+          project-specific support from{' '}
+          <a
+            href="https://www.banxico.org.mx/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Banco de México
+          </a>
+          , the{' '}
+          <a
+            href="https://www.kellogg.northwestern.edu/academics-research/global-poverty-research-lab/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Global Poverty Research Lab
+          </a>
+          , and the{' '}
+          <a
+            href="https://buffett.northwestern.edu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Roberta Buffett Institute for Global Affairs
+          </a>
+          .
           <br />
           <br /> Academically, I&apos;m interested in using novel data
-          collection and analysis methods to understand how households make
-          financial decisions. Personally, I&apos;m interested in hiking,
-          cycling, mountain biking, climbing, and obsessing over the perfect
-          espresso. I graduated from UC Berkeley in 2021 with dual degrees in
-          Economics and Data Science.
+          collection and analysis methods to understand how firms compete in
+          markets for consumer financial products. Personally, I&apos;m
+          interested in hiking, cycling, mountain biking, climbing, and
+          obsessing over the perfect espresso. I graduated from UC Berkeley in
+          2021 with dual degrees in Economics and Data Science.
         </div>
         <br /> <span style={{ fontWeight: 'bold' }}>Email:</span>{' '}
         [first].[last]@kellogg.northwestern.edu
@@ -54,10 +87,16 @@ const Home = () => (
           <IconButton
             className="mr-2"
             href="https://www.linkedin.com/in/daniel-cohen-12312a140"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <LinkedinFilled />
           </IconButton>
-          <IconButton href="https://github.com/dannyboy2233">
+          <IconButton
+            href="https://github.com/dannyboy2233"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <GithubFilled />
           </IconButton>
         </div>
